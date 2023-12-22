c44d1f234a6b2669735d6279943f386f83670d2e
Mario Esteban Espitia Martinez <71477601+marckes04@users.noreply.github.com>
2023-12-22 13:24:51 -0500

    transition working

--- Assets/Scripts/System/GameManager.cs
+++ Assets/Scripts/System/GameManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class GameManager : MonoBehaviour
 {
-    public void Start()
+   public void GameStart()
     {
-        
+        SceneManager.LoadScene("MainLevel");
     }
 }
