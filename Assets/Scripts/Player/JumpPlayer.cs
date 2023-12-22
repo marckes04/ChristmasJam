9f9c7dc9494d299fa05525a37fe0bd569c6ef93d
nidhi-u <nidhiuppoor@gmail.com>
2023-12-23 01:22:36 +0530

    Jumping anim, gravity and double jumping

--- Assets/Scripts/Player/JumpPlayer.cs
+++ Assets/Scripts/Player/JumpPlayer.cs
@@ -5,14 +5,34 @@ using UnityEngine.InputSystem;
 
 public class JumpPlayer : MonoBehaviour
 {
+
+    private PlayerControls input = null;
+    private Rigidbody2D rb;
     public bool isGrounded;
     public float groundCheckDistance;
     private float bufferCheckDistance = 0.1f;
-    private PlayerControls input = null;
+
+
+    [Header("Ground Check")]
+    [SerializeField] Transform groundCheckPos;
+    [SerializeField] Vector2 groundCheckSize;
+    public LayerMask groundLayer;
+
+
+    [Header("Jumping")]
     public float jumpForce = 10f;
-    private Animator myAnim;
     private bool isJumping;
-    private Rigidbody2D rb;
+    private int maxJumps = 2;
+    private int jumpsRemaining;
+
+    [Header("Animation")]
+    private Animator myAnim;
+
+    // Adding gravity acceleration while falling
+    [Header("Gravity")]
+    [SerializeField] float baseGravity;
+    [SerializeField] float maxFallSpeed;
+    [SerializeField] float fallSpeedMultiplier;
 
     private void Awake()
     {
@@ -38,7 +58,7 @@ public class JumpPlayer : MonoBehaviour
 
     private void FixedUpdate()
     {
-        
+
 
         groundCheckDistance = (GetComponent<CapsuleCollider2D>().size.y / 2) + bufferCheckDistance;
 
@@ -48,7 +68,6 @@ public class JumpPlayer : MonoBehaviour
         {
             isGrounded = true;
             isJumping = true;
-
         }
         else
         {
@@ -56,14 +75,19 @@ public class JumpPlayer : MonoBehaviour
             isJumping = false;
         }
 
+        checkGrounded();
+
+        Gravity();
     }
 
     private void OnJumpStarted(InputAction.CallbackContext context)
     {
-        if (isJumping)
+        jumpsRemaining--;
+        if (isJumping && jumpsRemaining > 0)
         {
             Jump();
         }
+
     }
 
     private void OnJumpCanceled(InputAction.CallbackContext context)
@@ -73,6 +97,34 @@ public class JumpPlayer : MonoBehaviour
 
     private void Jump()
     {
-        GetComponent<Rigidbody2D>().AddForce(Vector3.up * jumpForce,ForceMode2D.Impulse);
+        myAnim.Play("Jump");
+        GetComponent<Rigidbody2D>().AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void Gravity()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.gravityScale = baseGravity * fallSpeedMultiplier; // fall increasing faster
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Max(rb.velocity.y, -maxFallSpeed));
+        }
+        else
+        {
+            rb.gravityScale = baseGravity;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireCube(groundCheckPos.position, groundCheckSize);
+    }
+
+    private void checkGrounded()
+    {
+        if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer))
+        {
+            jumpsRemaining = maxJumps;
+        }
     }
 }
